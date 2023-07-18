9081383f26e33d9e8a1cc33f17608971d1970ccd
Vectorized <webby1111@hotmail.com>
2023-07-18 17:20:42 +0000

    Tidy

--- build_multicaller_with_signer.sh
+++ build_multicaller_with_signer.sh
@@ -16,18 +16,18 @@ rm .tmp/foundry.toml > /dev/null 2>&1;
 
 forge build --out="out" --root=".tmp" --contracts="." --via-ir --optimize --optimizer-runs=200 --use=0.8.18;
 
-mkdir multicaller_with_sender > /dev/null 2>&1;
+mkdir multicaller_with_signer > /dev/null 2>&1;
 
 echo 'const fs = require("fs"), rfs = s => fs.readFileSync(s, { encoding: "utf8", flag: "r" });
 fs.writeFileSync(
-    "multicaller_with_sender/initcode.txt", 
+    "multicaller_with_signer/initcode.txt", 
     JSON.parse(rfs(".tmp/out/MulticallerWithSigner.sol/MulticallerWithSigner.json"))["bytecode"]["object"].slice(2)
 )' > .tmp/extract_initcode.js;
 node .tmp/extract_initcode.js;
 
 echo 'const fs = require("fs"), rfs = s => fs.readFileSync(s, { encoding: "utf8", flag: "r" });
 fs.writeFileSync(
-    "multicaller_with_sender/input.json", 
+    "multicaller_with_signer/input.json", 
     JSON.stringify({
         "language": "Solidity",
         "sources": {
@@ -50,12 +50,12 @@ if [ ! -f .tmp/package-lock.json ]; then cd .tmp; npm install; cd ..; fi
 
 echo 'const fs = require("fs"), rfs = s => fs.readFileSync(s, { encoding: "utf8", flag: "r" });
 fs.writeFileSync(
-    "multicaller_with_sender/initcodehash.txt", 
-    require("@ethersproject/keccak256").keccak256("0x" + rfs("multicaller_with_sender/initcode.txt"))
+    "multicaller_with_signer/initcodehash.txt", 
+    require("@ethersproject/keccak256").keccak256("0x" + rfs("multicaller_with_signer/initcode.txt"))
 )' > .tmp/generate_initcodehash.js;
 node .tmp/generate_initcodehash.js;
 
 rm .tmp/*.sol .tmp/*.js > /dev/null 2>&1;
 
-cat multicaller_with_sender/initcodehash.txt; echo;
-cat multicaller_with_sender/initcode.txt; echo;
+cat multicaller_with_signer/initcodehash.txt; echo;
+cat multicaller_with_signer/initcode.txt; echo;
