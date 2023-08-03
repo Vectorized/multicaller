18a14c28b663a5ef94aab6174921198097159a02
Vectorized <webby1111@hotmail.com>
2023-08-03 18:57:11 +0800

    ⚡️ Optimize (#23)

--- src/Multicaller.sol
+++ src/Multicaller.sol
@@ -39,28 +39,6 @@ contract Multicaller {
         address refundTo
     ) external payable returns (bytes[] memory) {
         assembly {
-            // Force transfers all the remaining ETH in the contract to `to`,
-            // with a gas stipend of 100000, which should be enough for most use cases.
-            // If sending via a regular call fails, force sends the ETH by
-            // creating a temporary contract which uses `SELFDESTRUCT` to force send the ETH.
-            function forceSafeRefundETH(to) {
-                if iszero(selfbalance()) { leave } // If there's no remaining ETH, leave.
-                // If `to` is `address(1)`, replace it with the `msg.sender`.
-                to := xor(to, mul(eq(to, 1), xor(to, caller())))
-                // Transfer the ETH and check if it succeeded or not.
-                if iszero(call(100000, to, selfbalance(), 0x00, 0x00, 0x00, 0x00)) {
-                    mstore(0x00, to) // Store the address in scratch space.
-                    mstore8(0x0b, 0x73) // Opcode `PUSH20`.
-                    mstore8(0x20, 0xff) // Opcode `SELFDESTRUCT`.
-                    // We can directly use `SELFDESTRUCT` in the contract creation.
-                    // Compatible with `SENDALL`: https://eips.ethereum.org/EIPS/eip-4758
-                    if iszero(create(selfbalance(), 0x0b, 0x16)) {
-                        // Coerce gas estimation to provide enough gas for the `create` above.
-                        if iszero(gt(gas(), 1000000)) { revert(0x00, 0x00) }
-                    }
-                }
-            }
-
             if iszero(and(eq(targets.length, data.length), eq(data.length, values.length))) {
                 // Store the function selector of `ArrayLengthsMismatch()`.
                 mstore(returndatasize(), 0x3b800a46)
@@ -68,69 +46,89 @@ contract Multicaller {
                 revert(0x1c, 0x04)
             }
 
-            // Early return if no data.
-            if iszero(data.length) {
-                if refundTo { forceSafeRefundETH(refundTo) }
-                mstore(0x00, 0x20) // Store the memory offset of the `results`.
-                mstore(0x20, targets.length) // Store `targets.length` into `results`.
-                return(0x00, 0x40)
-            }
+            let resultsSize := 0x40
 
-            let results := 0x40
-            // Left shift by 5 is equivalent to multiplying by 0x20.
-            data.length := shl(5, data.length)
-            // Copy the offsets from calldata into memory.
-            calldatacopy(results, data.offset, data.length)
-            // Offset into `results`.
-            let resultsOffset := data.length
-            // Pointer to the end of `results`.
-            let end := add(results, data.length)
-            // For deriving the calldata offsets from the `results` pointer.
-            let valuesOffsetDiff := sub(values.offset, results)
-            let targetsOffsetDiff := sub(targets.offset, results)
+            if data.length {
+                let results := 0x40
+                // Left shift by 5 is equivalent to multiplying by 0x20.
+                data.length := shl(5, data.length)
+                // Copy the offsets from calldata into memory.
+                calldatacopy(results, data.offset, data.length)
+                // Offset into `results`.
+                let resultsOffset := data.length
+                // Pointer to the end of `results`.
+                let end := add(results, data.length)
+                // For deriving the calldata offsets from the `results` pointer.
+                let valuesOffsetDiff := sub(values.offset, results)
+                let targetsOffsetDiff := sub(targets.offset, results)
 
-            for {} 1 {} {
-                // The offset of the current bytes in the calldata.
-                let o := add(data.offset, mload(results))
-                let memPtr := add(resultsOffset, 0x40)
-                // Copy the current bytes from calldata to the memory.
-                calldatacopy(
-                    memPtr,
-                    add(o, 0x20), // The offset of the current bytes' bytes.
-                    calldataload(o) // The length of the current bytes.
-                )
-                if iszero(
-                    call(
-                        gas(), // Remaining gas.
-                        calldataload(add(targetsOffsetDiff, results)), // Address to call.
-                        calldataload(add(valuesOffsetDiff, results)), // ETH to send.
-                        memPtr, // Start of input calldata in memory.
-                        calldataload(o), // Size of input calldata.
-                        0x00, // We will use returndatacopy instead.
-                        0x00 // We will use returndatacopy instead.
+                for {} 1 {} {
+                    // The offset of the current bytes in the calldata.
+                    let o := add(data.offset, mload(results))
+                    let memPtr := add(resultsOffset, 0x40)
+                    // Copy the current bytes from calldata to the memory.
+                    calldatacopy(
+                        memPtr,
+                        add(o, 0x20), // The offset of the current bytes' bytes.
+                        calldataload(o) // The length of the current bytes.
                     )
-                ) {
-                    // Bubble up the revert if the call reverts.
-                    returndatacopy(0x00, 0x00, returndatasize())
-                    revert(0x00, returndatasize())
+                    if iszero(
+                        call(
+                            gas(), // Remaining gas.
+                            calldataload(add(targetsOffsetDiff, results)), // Address to call.
+                            calldataload(add(valuesOffsetDiff, results)), // ETH to send.
+                            memPtr, // Start of input calldata in memory.
+                            calldataload(o), // Size of input calldata.
+                            0x00, // We will use returndatacopy instead.
+                            0x00 // We will use returndatacopy instead.
+                        )
+                    ) {
+                        // Bubble up the revert if the call reverts.
+                        returndatacopy(0x00, 0x00, returndatasize())
+                        revert(0x00, returndatasize())
+                    }
+                    // Append the current `resultsOffset` into `results`.
+                    mstore(results, resultsOffset)
+                    // Append the returndatasize, and the returndata.
+                    mstore(memPtr, returndatasize())
+                    returndatacopy(add(memPtr, 0x20), 0x00, returndatasize())
+                    // Advance the `resultsOffset` by `returndatasize() + 0x20`,
+                    // rounded up to the next multiple of 0x20.
+                    resultsOffset := and(add(add(resultsOffset, returndatasize()), 0x3f), not(0x1f))
+                    // Advance the `results` pointer.
+                    results := add(results, 0x20)
+                    if eq(results, end) { break }
                 }
-                // Append the current `resultsOffset` into `results`.
-                mstore(results, resultsOffset)
-                // Append the returndatasize, and the returndata.
-                mstore(memPtr, returndatasize())
-                returndatacopy(add(memPtr, 0x20), 0x00, returndatasize())
-                // Advance the `resultsOffset` by `returndatasize() + 0x20`,
-                // rounded up to the next multiple of 0x20.
-                resultsOffset := and(add(add(resultsOffset, returndatasize()), 0x3f), not(0x1f))
-                // Advance the `results` pointer.
-                results := add(results, 0x20)
-                if eq(results, end) { break }
+                resultsSize := add(resultsOffset, 0x40)
             }
-            if refundTo { forceSafeRefundETH(refundTo) }
+
+            if refundTo {
+                // Force transfers all the remaining ETH in the contract to `refundTo`,
+                // with a gas stipend of 100000, which should be enough for most use cases.
+                // If sending via a regular call fails, force sends the ETH by
+                // creating a temporary contract which uses `SELFDESTRUCT` to force send the ETH.
+                if selfbalance() {
+                    // If `refundTo` is `address(1)`, replace it with the `msg.sender`.
+                    refundTo := xor(refundTo, mul(eq(refundTo, 1), xor(refundTo, caller())))
+                    // Transfer the ETH and check if it succeeded or not.
+                    if iszero(call(100000, refundTo, selfbalance(), 0x00, 0x00, 0x00, 0x00)) {
+                        mstore(0x00, refundTo) // Store the address in scratch space.
+                        mstore8(0x0b, 0x73) // Opcode `PUSH20`.
+                        mstore8(0x20, 0xff) // Opcode `SELFDESTRUCT`.
+                        // We can directly use `SELFDESTRUCT` in the contract creation.
+                        // Compatible with `SENDALL`: https://eips.ethereum.org/EIPS/eip-4758
+                        if iszero(create(selfbalance(), 0x0b, 0x16)) {
+                            // Coerce gas estimation to provide enough gas for the `create` above.
+                            if iszero(gt(gas(), 1000000)) { revert(0x00, 0x00) }
+                        }
+                    }
+                }
+            }
+
             mstore(0x00, 0x20) // Store the memory offset of the `results`.
             mstore(0x20, targets.length) // Store `targets.length` into `results`.
             // Direct return.
-            return(0x00, add(resultsOffset, 0x40))
+            return(0x00, resultsSize)
         }
     }
 
