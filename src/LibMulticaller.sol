878db87ea0262ceaf13b3c8f335cb5ee057e57d8
Vectorized <webby1111@hotmail.com>
2024-04-09 00:17:07 +0700

    ✨ Adds a means to bypass fixed addresses for LibMulticaller (#35)
    * ✨ Adds a means to bypass fixed addresses for LibMulticaller (#34)
    
    Co-authored-by: 3commascapital <3commascapital@github.com>
    
    * Tidy
    
    ---------
    
    Co-authored-by: 3commascapital <90629478+3commascapital@users.noreply.github.com>
    Co-authored-by: 3commascapital <3commascapital@github.com>

--- src/LibMulticaller.sol
+++ src/LibMulticaller.sol
@@ -36,28 +36,14 @@ library LibMulticaller {
      * @dev Returns the caller of `aggregateWithSender` on `MULTICALLER_WITH_SENDER`.
      */
     function multicallerSender() internal view returns (address result) {
-        /// @solidity memory-safe-assembly
-        assembly {
-            mstore(0x00, 0x00)
-            if iszero(staticcall(gas(), MULTICALLER_WITH_SENDER, codesize(), 0x00, 0x00, 0x20)) {
-                revert(codesize(), codesize()) // For better gas estimation.
-            }
-            result := mload(0x00)
-        }
+        return at(MULTICALLER_WITH_SENDER);
     }
 
     /**
      * @dev Returns the signer of `aggregateWithSigner` on `MULTICALLER_WITH_SIGNER`.
      */
     function multicallerSigner() internal view returns (address result) {
-        /// @solidity memory-safe-assembly
-        assembly {
-            mstore(0x00, 0x00)
-            if iszero(staticcall(gas(), MULTICALLER_WITH_SIGNER, codesize(), 0x00, 0x00, 0x20)) {
-                revert(codesize(), codesize()) // For better gas estimation.
-            }
-            result := mload(0x00)
-        }
+        return at(MULTICALLER_WITH_SIGNER);
     }
 
     /**
@@ -66,17 +52,7 @@ library LibMulticaller {
      *      Otherwise, returns `msg.sender`.
      */
     function sender() internal view returns (address result) {
-        /// @solidity memory-safe-assembly
-        assembly {
-            mstore(0x00, caller())
-            let withSender := MULTICALLER_WITH_SENDER
-            if eq(caller(), withSender) {
-                if iszero(staticcall(gas(), withSender, codesize(), 0x00, 0x00, 0x20)) {
-                    revert(codesize(), codesize()) // For better gas estimation.
-                }
-            }
-            result := mload(0x00)
-        }
+        return resolve(MULTICALLER_WITH_SENDER);
     }
 
     /**
@@ -84,13 +60,35 @@ library LibMulticaller {
      *      if the current context's `msg.sender` is `MULTICALLER_WITH_SIGNER`.
      *      Otherwise, returns `msg.sender`.
      */
-    function signer() internal view returns (address result) {
+    function signer() internal view returns (address) {
+        return resolve(MULTICALLER_WITH_SIGNER);
+    }
+
+    /**
+     * @dev Returns the caller or signer at `a`.
+     * @param a The multicaller with sender / signer.
+     */
+    function at(address a) internal view returns (address result) {
+        /// @solidity memory-safe-assembly
+        assembly {
+            mstore(0x00, 0x00)
+            if iszero(staticcall(gas(), a, codesize(), 0x00, 0x00, 0x20)) {
+                revert(codesize(), codesize()) // For better gas estimation.
+            }
+            result := mload(0x00)
+        }
+    }
+
+    /**
+     * @dev Returns the caller or signer at `a`, if the caller is `a`.
+     * @param a The multicaller with sender / signer.
+     */
+    function resolve(address a) internal view returns (address result) {
         /// @solidity memory-safe-assembly
         assembly {
             mstore(0x00, caller())
-            let withSigner := MULTICALLER_WITH_SIGNER
-            if eq(caller(), withSigner) {
-                if iszero(staticcall(gas(), withSigner, codesize(), 0x00, 0x00, 0x20)) {
+            if eq(caller(), a) {
+                if iszero(staticcall(gas(), a, codesize(), 0x00, 0x00, 0x20)) {
                     revert(codesize(), codesize()) // For better gas estimation.
                 }
             }
